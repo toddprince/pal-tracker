1390cf40b149200bbea8f4eba7ae703aedb4bfbc
toddprince <todd.prince@pobox.com>
2021-04-27 20:00:09 +0000

    lab 4 mvc

---
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
@@ -0,0 +1,54 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/time-entries")
+public class TimeEntryController {
+    private TimeEntryRepository timeEntryRepository;
+    public TimeEntryController(TimeEntryRepository timeEntryRepository) {
+        this.timeEntryRepository = timeEntryRepository;
+    }
+
+    @PostMapping()
+    public ResponseEntity<TimeEntry> create(@RequestBody TimeEntry timeEntryToCreate) {
+        TimeEntry timeEntry = timeEntryRepository.create(timeEntryToCreate);
+        return ResponseEntity.created(null).body(timeEntry);
+    }
+
+    @GetMapping("/{timeEntryId}")
+    public ResponseEntity<TimeEntry> read(@PathVariable long timeEntryId) {
+        TimeEntry timeEntry = timeEntryRepository.find(timeEntryId);
+        if (timeEntry != null) {
+            return ResponseEntity.ok(timeEntry);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TimeEntry>> list() {
+        List<TimeEntry> list = timeEntryRepository.list();
+        return ResponseEntity.ok(list);
+    }
+
+    @PutMapping("/{timeEntryId}")
+    public ResponseEntity<TimeEntry> update(@PathVariable long timeEntryId, @RequestBody TimeEntry timeEntryToUpdate) {
+        TimeEntry timeEntry = timeEntryRepository.update(timeEntryId, timeEntryToUpdate);
+        if (timeEntry != null) {
+            return ResponseEntity.ok(timeEntry);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{timeEntryId}")
+    public ResponseEntity<Void> delete(@PathVariable long timeEntryId) {
+        timeEntryRepository.delete(timeEntryId);
+        return ResponseEntity.noContent().build();
+    }
+}
